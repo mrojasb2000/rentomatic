0fb7323eb168b03bc99b5ef308f7637f0793e51d
mrojasb2000 <mrojasb2000@gmail.com>
2023-03-29 21:15:58 -0300

    chore: add room list use case

---
+++ tests/use_cases/test_room_list.py
@@ -0,0 +1,54 @@
+import pytest
+import uuid
+from unittest import mock
+
+from  rentomatic.domain.room  import  Room 
+from  rentomatic.use_cases.room_list  import  room_list_use_case 
+
+@pytest . fixture
+def domain_rooms():
+    room_1 = Room(
+       code=uuid.uuid4(),
+       size=215,
+       price=39,
+       longitude=-0.09998975,
+       latitude=51.75436293
+    )
+
+    room_2 = Room(
+       code=uuid.uuid4(),
+       size=405,
+       price=66,
+       longitude=-0.09998975,
+       latitude=51.75436293
+    )
+
+    room_3 = Room(
+       code=uuid.uuid4(),
+       size=56,
+       price=60,
+       longitude=-0.09998975,
+       latitude=51.75436293
+    )
+
+    room_4 = Room(
+       code=uuid.uuid4(),
+       size=93,
+       price=48,
+       longitude=-0.09998975,
+       latitude=51.75436293
+    )
+
+    return [room_1, room_2, room_3, room_4]
+
+def test_list_rooms_without_params(domain_rooms):
+   repo = mock.Mock()
+   repo.list.return_value = domain_rooms
+   
+   # Initialize the use case with the repository and execute it. 
+   result = room_list_use_case(repo)
+   
+   # Check is that the repository method was called without any params.
+   repo.list.assert_called_with()
+
+   assert result == domain_rooms
